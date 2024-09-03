ba3fd5911cde00a7a379c94b4c6d553c77a8a326
Joshithamaddena <153305356+Joshithamaddena@users.noreply.github.com>
2024-09-03 18:53:49 +0530

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './App';
+import theme from './theme';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <App />
+      </Router>
+    </ChakraProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
